197a4321ba51ca9e06e2eebed83f8a8101881fc7
IvanMonichev <risen15@yandex.ru>
2025-04-19 16:22:59 +0300

    feat: Create TWC infrastructure for story stream (main)
    This commit creates the necessary TWC infrastructure
    for the story stream application. It includes:
    
    - Configuration of the TWC provider with project.
    - Creation of a VPC named "Story Stream VPC".
    - Creation of a server based on Ubuntu 22.04.
    - Assigning a static IP address to this server.
    - Example usage of adding an additional IP address.

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,8 @@ terraform {
 
 
 provider "twc" {
-  token = var.api_token
+  project = "story-stream-vm"
+  token   = var.api_token
 }
 
 data "twc_configurator" "configurator" {
@@ -22,23 +23,24 @@ data "twc_os" "ubuntu" {
   version = "22.04"
 }
 
-
 resource "twc_vpc" "story-stream-vpc" {
-  name = "Story Stream VPC"
+  name      = "Story Stream VPC"
   subnet_v4 = "192.168.0.0/24"
-  main_ipv4 = "188.225.46.223"
-  location = "ru-1"
+  location  = "ru-1"
 }
 
-resource "twc_server" "v01" {
-  name  = "Server Terraform"
-  os_id = data.twc_os.ubuntu.id
 
-  local_network {
-    id = twc_vpc.story-stream-vpc.id
-    ip = "192.168.0.15"
+data "twc_presets" "twc-preset" {
+  price_filter {
+    from = 300
+    to   = 400
   }
+}
 
+resource "twc_server" "story-stream-server" {
+  name      = "Server Terraform"
+  os_id     = data.twc_os.ubuntu.id
+  preset_id = data.twc_presets.twc-preset.id
 
   configuration {
     configurator_id = data.twc_configurator.configurator.id
@@ -51,3 +53,10 @@ resource "twc_server" "v01" {
 
 }
 
+# Usage example for create additional IP address
+resource "twc_server_ip" "example" {
+  source_server_id = twc_server.story-stream-server.id
+
+  type = "ipv4"
+  ptr  = "188.225.46.223"
+}
